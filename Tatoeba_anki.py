b789d61355682fc0506776277eb0f4628fdaf370
Константин Неклюдов <nekludov@fenix-it.ru>
2023-10-23 20:41:32 +0300

    Using api while process a sentence

--- Tatoeba_anki.py
+++ Tatoeba_anki.py
@@ -9,17 +9,18 @@ import csv
 import os
 import shutil
 import json
+import datetime
 
 #######################################
 # basic variables you can change
 #######################################
 
-UrlListOfSentences = 'https://tatoeba.org/eng/sentences/search?query=&from=spa&to=und&user=&orphans=no&unapproved=no&has_audio=yes&tags=&list=&native=yes&trans_filter=limit&trans_to=rus&trans_link=direct&trans_user=&trans_orphan=&trans_unapproved=&trans_has_audio=&sort=words&sort_reverse=' # basic url with the list of the sentences (if there are many pages they will be processed page by page)
+UrlListOfSentences = 'https://tatoeba.org/ru/sentences_lists/show/7407/eng/rus' # basic url with the list of the sentences (if there are many pages they will be processed page by page)
 getAudio = True # True if we grab audio if sentences in a source language have it
 getTags = True # True if we copy the tags if they exist
 getAutor = True # True if we want to know who is the author (will appear as an extra tag)
-srclang = ["spa"] # languages of source sentences (may be 1 or more that will appear on the question field). This should be 2-letter code ISO 639-1  https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes
-audio3letterslangcodes = ["spa","eng"] # these codes are used in Tatoeba's url when you browse audio by language, e.g. cmn for Chinese in https://tatoeba.org/eng/sentences/with_audio/cmn
+srclang = ["eng"] # languages of source sentences (may be 1 or more that will appear on the question field). This should be 2-letter code ISO 639-1  https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes
+audio3letterslangcodes = ["eng"] # these codes are used in Tatoeba's url when you browse audio by language, e.g. cmn for Chinese in https://tatoeba.org/eng/sentences/with_audio/cmn
 targetlang = "rus" # target language code (will be on the answer's fiels)
 copymediafilestoankifolder = True # if true you should manually set your anki media folder
 #ankimediafolder = "C:\\Users\\atomi\\AppData\\Roaming\\Anki2\\Miles\\collection.media"  # this is "collection.media" folder which is normally located in your documents folder (in Windows)
@@ -48,12 +49,10 @@ def procstring(string):
     res=res.replace("&quot;",'"')
     return res
 
-# process the link, open it and grab all we need
-def proclink(num):
+def getTags(num):
     taglist = []
     url = 'https://tatoeba.org/eng/sentences/show/' + num
 
-    curaudio = ''
     resp = urllib.request.urlopen(url)
     if resp.getcode() != 200:
         print("Error response for search")
@@ -61,44 +60,59 @@ def proclink(num):
     html = resp.read().decode('utf-8')
 
     if getTags:
-        tagname=re.findall('class="tagName".+?\>(.+?)\<',html,re.DOTALL)
+        tagname = re.findall('class="tagName".+?\>(.+?)\<', html, re.DOTALL)
         for i in tagname:
-            taglist.append(i.strip().replace(" ","_"))
-    #print(taglist)
+            taglist.append(i.strip().replace(" ", "_"))
 
-    jsonSentence = re.findall('<div ng-cloak flex.+?sentence-and-translations.+?ng-init="vm.init\(\[\]\,(.+?}), \[\{',procstring(html), re.DOTALL)
+
+# process the link, open it and grab all we need
+def proclink(num):
+    if getTags:
+        taglist = getTags(num)
+    if not taglist:
+        taglist = []
+    curaudio = ''
+
+    url = 'https://tatoeba.org/eng/api_v0/sentence/' + num
+    resp = urllib.request.urlopen(url)
+    if resp.getcode() != 200:
+        print("Error response for search")
+        sys.exit(1)
+    html = resp.read().decode('utf-8')
+
+    jsonSentence = json.loads(html)
     targetsentence = ''
     srcsentence = ''
 
-    for jsonItem in jsonSentence:
-        #print(jsonItem)
-        jsonData = json.loads(jsonItem)
-        if getAutor:
-            authorname = jsonData['user']['username']
-            if len(authorname) > 0:
-                taglist.append('by_' + authorname)
-            else:
-                taglist.append('orphan_sentence')
-        mainlang = jsonData['lang']
-        if mainlang in srclang:
-            srcsentence = jsonData['text']
-            for translations in jsonData['translations']:
-                for translation in translations:
-                    if targetlang == translation['lang'] and 'isDirect' in translation and translation['isDirect']:
-                        targetsentence = translation['text']
-                        #print(targetsentence)
+    jsonData = jsonSentence
+    if getAutor:
+        authorname = jsonData['user']['username']
+        if len(authorname) > 0:
+            taglist.append('by_' + authorname)
+        else:
+            taglist.append('orphan_sentence')
+    mainlang = jsonData['lang']
+    if mainlang in srclang:
+        srcsentence = jsonData['text']
+        for translations in jsonData['translations']:
+            for translation in translations:
+                if targetlang == translation['lang'] and 'isDirect' in translation and translation['isDirect']:
+                    targetsentence = translation['text']
+                    #print(targetsentence)
 
-    if srcsentence=='':
-        print("Error while trying to get the source sentence")
+    if not targetsentence:
         return
 
-    audiourl = 'https://audio.tatoeba.org/sentences/' + mainlang + '/' + num + '.mp3'
     if getAudio:
-        if 'audioAvailable' in html:
+        if jsonData['audios']:
+            audiourl = 'https://tatoeba.org/' + mainlang + '/audio/download/' + str(jsonData['audios'][0]['id'])
             # grab audio
-            urllib.request.urlretrieve(audiourl,"foranki/" + num + ".mp3")
-            curaudio = '[sound:' + num + '.mp3]'
+            urllib.request.urlretrieve(audiourl, "foranki/" + str(num) + ".mp3")
+            curaudio = '[sound:' + str(num) + '.mp3]'
 
+    if srcsentence == '':
+        print("Error while trying to get the source sentence")
+        return
 
     csv_writer = csv.writer(cfile, delimiter='\t', lineterminator='\n')
     #print(" ".join([srcsentence + curaudio, targetsentence, " ".join(taglist)]))
@@ -145,7 +159,7 @@ def mainproc():
 
     prCnt = 1 # this is a progress counter (not really necessary but kind of convenient feature)
 
-    for pagescounter in range(2,pagescount + 1):
+    for pagescounter in range(2, pagescount + 1):
         # page=2 ?page=2
         delim = '?'
         if '?' in UrlListOfSentences:
@@ -175,7 +189,8 @@ def mainproc():
         prCnt += 1
         curPrcnt = (100.0*prCnt) / pagescount
         #os.system('title ' + str(round(curPrcnt,3)) + '% completed')
-        print(str(round(curPrcnt,3)) + '% completed')
+        when = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+        print(when + ' ' + str(round(curPrcnt, 3)) + '% completed ' + '{0}/{1}'.format(prCnt, pagescount))
 
     # copy media files to anki media folder
     for root, dirs, files in os.walk('foranki'):
@@ -183,7 +198,7 @@ def mainproc():
             filename = os.path.join(root, f)
             if filename.endswith('.mp3'):
                 if copymediafilestoankifolder:
-                    shutil.copy2(filename,ankimediafolder)
+                    shutil.copy2(filename, ankimediafolder)
 
 mainproc()
 cfile.close()
